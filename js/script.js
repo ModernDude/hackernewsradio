96b655716d12b843c8b16f6b7434e96a7235e609
Jeff Sigmon <jeff@jeffsigmon.com>
2011-12-24 21:57:42 -0600

    smoke!

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-(function ($) {
-
-
-    
+(function ($, smoke) {
     $(function (){
 	$('a').smoothScroll();
 	$('audio').mediaelementplayer({features: ['playpause','current','volume','fullscreen']});
 
-
+	if (smoke) {
+	    smoke.imgLocation = "img/smoke.png";
+            smoke.smokeWidth = 130;
+            smoke.smokeHeight = 100;
+            smoke.makeEffect("smoke", 0, 0);
+	}
   });
-
-}(jQuery));
+}(jQuery, SmokeEffect));
 
 
 
